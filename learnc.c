8ac70ef8248709f24fed3f72f2d4f44e5634bdb0
Neous1 <Neous1@users.noreply.github.com>
2017-10-24 17:27:39 -0400

    using %d for integer

---
+++ learnc.c
@@ -0,0 +1,28 @@
+/*
+Multiline
+comment
+*/
+
+// single line comment
+
+#include <stdio.h>
+#include <string.h>
+
+#define MYNAME "Yvon N'Chonon";
+
+int globalVar = 100;
+
+main(){
+    char firstLetter = 'Y';
+
+    int age = 38;
+
+    long int superBigNum = -327670000;
+
+    float piValue = 3.14159;
+
+    double reallyBigPi = 31.14151979284421;
+
+     printf("\n");
+     printf("This will print to screen \n\n");
+}
