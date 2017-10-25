420f96ca673f92ec6ca0191ef2813166024f8687
Neous1 <Neous1@users.noreply.github.com>
2017-10-25 12:10:10 -0400

    operators

---
+++ ctutorial2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+    /* to compile your code run make programName (without the extension 
+    then run ./programName
+    now go debugg :-)    
+    */
+
+    /*thre are many ways to compare datra in c >, <, ==, >=, <=, !=
+    only compare values wiht the same data type
+    to comapre 2 unlike types perform a cast
+    A relational operator alwys evaluates to 1 for true, or 0 for false   
+    */
+    printf("\n");
+
+    int num1= 1, num2 = 2; 
+    
+    printf ("Is 1 > 2 : %d\n\n", num1>num2);
+}
